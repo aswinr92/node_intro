76ed27fa6c89df85ab7a0dfba69cea3e64c86c8c
Aswin Radhakrishnan <aswinr1992@gmail.com>
2015-05-22 11:48:14 +0000

    BAsic server setup

---
+++ server/basic.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    
+    if(req.url=== '/file.txt'){
+        fs.createReadStream(__dirname+'/file.txt').pipe(res);
+    }
+    else{
+        res.end('Hello');
+    }
+}).listen(80);
+
+console.log('Server Running!!!');
